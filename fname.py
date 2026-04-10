2eb2079fc3db7846a98d3fd8b3dd0dab8db209e4
Amy Farmer <af1226@york.ac.uk>
2026-04-10 16:43:03 +0100

    Added file paths on linux for ATHENA

--- fname.py
+++ fname.py
@@ -10,10 +10,14 @@ def get_fname():
     if name == 'posix':
         fname_left = '/home/amy/data/LEFT detector/*.csv'
         fname_right = '/home/amy/data/RIGHT detector/*.csv'
+        fname_summer = '/home/amy/data/summer/*.csv'
+        fname_winter = '/home/amy/data/winter/*.csv'
     elif name == 'nt':
         fname_left = 'A:\Amy\Protein Library\LIFEtime library\LEFT detector\*.csv'
         fname_right = 'A:\Amy\Protein Library\LIFEtime library\RIGHT detector\*.csv'
+        fname_summer = 'A:\Amy\Protein Library\ATHENA\data_for_ML\summer\interpolated\*.csv'
+        fname_winter = 'A:\Amy\Protein Library\ATHENA\data_for_ML\winter\interpolated\*.csv'
     else:
         print ('Rogue OS! Stopping now.')
-    return fname_left, fname_right
+    return fname_left, fname_right, fname_summer, fname_winter
 
